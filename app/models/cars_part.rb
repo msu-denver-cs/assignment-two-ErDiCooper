42fdf026ea8a7efd36aea080b09bec5208cbca1d
ErDiCooper <ericdicarlo@hotmail.com>
2019-09-21 14:54:13 -0600

    Revert "Created CarsParts Scaffolding"
    This reverts commit c81c12b8d4e4912a395ad25578b809a5c80cc9d1.

--- app/models/cars_part.rb
+++
@@ -1,2 +0,0 @@
-class CarsPart < ApplicationRecord
-end
--- db/migrate/20190921204626_create_cars_parts.rb
+++
@@ -1,8 +0,0 @@
-class CreateCarsParts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :cars_parts do |t|
-
-      t.timestamps
-    end
-  end
-end
